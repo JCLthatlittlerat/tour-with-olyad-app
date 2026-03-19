c0caa6e9e9fc96d774d6b78c729149ba65fde7c5
Olyad Negero <olyadnegero@gmail.com>
2026-03-19 16:47:17 +0300

    Revert "responsiveness is applied to the project and it is awesome"
    This reverts commit 5a0734284c5527b46fec0cc44315ea0bc95f897e.

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,52 +1,46 @@
-import React from "react";
+import React from 'react'
 import Tour from "./Tour";
 
-function Tours({ data, removeOneItem, refresh }) {
-  if (!data || data.length === 0) {
-    return (
-      <div className="App">
-        <h1>No Tours Left</h1>
-        <button
-          className="refresh-btn"
-          onClick={() => {
-            refresh();
-          }}
-        >
-          Refresh
-        </button>
-      </div>
-    );
 
-    // return(
+function Tours({data, removeOneItem, refresh}) {
 
-    //   <h1>No Data found, <br /> Please check your Internet connection!</h1>
-    // )
+  if(!data || data.length === 0){
+      return (
+        < div className='App'>
+          <h1>No Tours Left</h1>
+          <button className='refresh-btn' onClick={()=>{refresh()}} >Refresh</button>
+        </div>
+      )
+  
+
+
+      // return(
+
+      //   <h1>No Data found, <br /> Please check your Internet connection!</h1> 
+      // )
+    }
+
+      return (
+  
+          <>
+          
+            <h1 className='our-tours'>Our Tours</h1>
+            <div className='underline'></div>
+
+          
+          <article>
+              
+            {data.map((tour) => {
+              return (<Tour key={tour.id} id={tour.id} image ={tour.image} info={tour.info} name={tour.name} price={tour.price } removeTour={removeOneItem} /> )
+            })
+            }
+            
+            {/* !data ? <Loading/> : data.map((data) =>{ return (<Tour key={data.id} info ={data} /> )}) */}
+  
+          </article>
+          
+          </>
+    )
   }
 
-  return (
-    <main>
-      <header>
-        <h1 className="our-tours">Our Tours</h1>
-        <div className="underline"></div>
-      </header>
-      <section className="cards grid grid-cols-2 sm:bg-red-50 border">
-        {data.map((tour) => {
-          return (
-            <Tour
-              key={tour.id}
-              id={tour.id}
-              image={tour.image}
-              info={tour.info}
-              name={tour.name}
-              price={tour.price}
-              removeTour={removeOneItem}
-            />
-          );
-        })}
-        {/* !data ? <Loading/> : data.map((data) =>{ return (<Tour key={data.id} info ={data} /> )}) */}
-      </section>
-    </main>
-  );
-}
-
-export default Tours;
+export default Tours

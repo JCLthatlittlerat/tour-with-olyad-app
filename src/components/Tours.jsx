f50e8eb38677ca98b99b50f04088db7594d230d4
Olyad Negero <olyadnegero@gmail.com>
2026-03-20 16:39:04 +0300

    The card container is made responsive, and it is awesome

--- src/components/Tours.jsx
+++ src/components/Tours.jsx
@@ -1,46 +1,52 @@
-import React from 'react'
+import React from "react";
 import Tour from "./Tour";
 
+function Tours({ data, removeOneItem, refresh }) {
+  if (!data || data.length === 0) {
+    return (
+      <div className="App">
+        <h1>No Tours Left</h1>
+        <button
+          className="refresh-btn"
+          onClick={() => {
+            refresh();
+          }}
+        >
+          Refresh
+        </button>
+      </div>
+    );
 
-function Tours({data, removeOneItem, refresh}) {
+    // return(
 
-  if(!data || data.length === 0){
-      return (
-        < div className='App'>
-          <h1>No Tours Left</h1>
-          <button className='refresh-btn' onClick={()=>{refresh()}} >Refresh</button>
-        </div>
-      )
-  
-
-
-      // return(
-
-      //   <h1>No Data found, <br /> Please check your Internet connection!</h1> 
-      // )
-    }
-
-      return (
-  
-          <>
-          
-            <h1 className='our-tours'>Our Tours</h1>
-            <div className='underline'></div>
-
-          
-          <article>
-              
-            {data.map((tour) => {
-              return (<Tour key={tour.id} id={tour.id} image ={tour.image} info={tour.info} name={tour.name} price={tour.price } removeTour={removeOneItem} /> )
-            })
-            }
-            
-            {/* !data ? <Loading/> : data.map((data) =>{ return (<Tour key={data.id} info ={data} /> )}) */}
-  
-          </article>
-          
-          </>
-    )
+    //   <h1>No Data found, <br /> Please check your Internet connection!</h1>
+    // )
   }
 
-export default Tours
+  return (
+    <>
+      <h1 className="our-tours">Our Tours</h1>
+      <div className="underline"></div>
+
+      <article className="cards">
+        {data.map((tour) => {
+          return (
+            <Tour
+              key={tour.id}
+              id={tour.id}
+              image={tour.image}
+              info={tour.info}
+              name={tour.name}
+              price={tour.price}
+              removeTour={removeOneItem}
+            />
+          );
+        })}
+
+        {/* !data ? <Loading/> : data.map((data) =>{ return (<Tour key={data.id} info ={data} /> )}) */}
+      </article>
+    </>
+  );
+}
+
+export default Tours;
